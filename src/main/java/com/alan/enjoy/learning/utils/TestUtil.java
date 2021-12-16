42e66aebd8f0bdc8cc95be83db7871c1809f43da
ecfgilt <liang.a.wu@ericsson.com>
2021-12-16 17:46:28 +0800

    Init enjoy-sonar

---
+++ src/main/java/com/alan/enjoy/learning/utils/TestUtil.java
@@ -0,0 +1,7 @@
+package com.alan.enjoy.learning.utils;
+
+public class TestUtil {
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/alan/enjoy/learning/utils/TestUtilTest.java
@@ -0,0 +1,11 @@
+package com.alan.enjoy.learning.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class TestUtilTest {
+
+    @Test
+    public void testHelloWorld(){
+        TestUtil.helloWorld();
+    }
+}
